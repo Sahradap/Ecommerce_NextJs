49e4892b841f275470203cc63bb765c3bf3ef4a4
sahradap <sahradap30@gmail.com>
2026-03-07 13:01:15 +0400

    updated pages

--- app/components/product-card/ProductCard.tsx
+++ app/components/product-card/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext} from "react";
 import "../special-buttons/SpecialButtons.css";
 import { CartContext } from "../context/CartContext";
 
